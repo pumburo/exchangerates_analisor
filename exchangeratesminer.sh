af4774a06632ba5745fc311a85a7236f72461412
pumburo <a.mahmutgur@gmail.com>
2021-06-16 17:47:08 +0000

    Commit-19

--- exchangeratesminer.sh
+++ exchangeratesminer.sh
@@ -6,7 +6,7 @@ find exchangerates > /dev/null
 fcreator=$( echo $? )
 if [[ $fcreator != 0 ]]; then
 	mkdir exchangerates
-fi	 
+fi
 for (( x = 1; x <= $line; x++ )); do
 	a=$( cat data.txt | head -n $x | tail -1 )
 	find ./exchangerates/$a.json 
@@ -17,7 +17,6 @@ for (( x = 1; x <= $line; x++ )); do
 		echo $a >> ./exchangerates/missingdates.txt
 	fi
 done
-##############################################################################################################
 newline=$( cat ./exchangerates/missingdates.txt | wc -l )
 for (( i = 0; i < $newline; i++ )); do
 	newdate=$( cat ./exchangerates/missingdates.txt | head -n $i | tail -1 )
@@ -33,20 +32,6 @@ for (( i = 0; i < $newline; i++ )); do
 	fi
 done
 rm ./exchangerates/missingdates.txt
-##############################################################################################################
-#for (( i = 0; i < $line; i++ )); do
-#	newdate=$( cat data.txt | head -n $i | tail -1 )
-#	curl -s https://api.exchangeratesapi.io/$newdate | python -m json.tool > ./exchangerates/$newdate.json
-#	cat ./exchangerates/$newdate.json | grep "There is no data" > /dev/null
-#	savereq=$( echo $? )
-#	if [[ $savereq == 0 ]]; then
-#		rm ./exchangerates/$newdate.json
-#	fi
-#	clear
-#	if [[ $savereq != 0 ]]; then
-#		cat ./exchangerates/$newdate.json
-#	fi
-#done
 clear
 echo "Sequence is complete. All json files saved in exchangerates folder."
 rm data.txt
